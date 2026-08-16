c5d003cf98b5eca51ff2aee2e82f061f89a1377f
nikib3x <felishacerpa01@gmail.com>
2026-08-16 04:09:29 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "", // required by Discord API, not shown in the client
         state: "Nikibabii's latest video",     // this is what people actually see
-        type: 3,               // Custom
+        type: 3,               // Nikibabii's latest video
       },
     ],
   },
