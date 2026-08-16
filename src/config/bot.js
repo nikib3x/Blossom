363760b9fcb5ea87a65b7b4735180e3b66b9a13b
nikib3x <felishacerpa01@gmail.com>
2026-08-16 04:35:45 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "", // required by Discord API, not shown in the client
-        state: "nikibabii's latest video",     // this is what people actually see
+        name: "nikibabii's latest video"", // required by Discord API, not shown in the client
+        state: "",     // this is what people actually see
         type: 3,            // 
       },
     ],
